ac5695984049aaf9129d58fe4361358b509edebc
moaath ibrahim <moaaz@gmail.com>
2024-10-14 17:26:27 -0700

    sleeping

--- src/App.js
+++ src/App.js
@@ -1,104 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import {useState} from 'react'
 
-function App() {
+export default function App() {
+  let [input, setInput] = useState({
+    formOne: "", 
+    formTwo: ""
+  });
+
+  const [devices, setDevices] = useState(["Mac", "Linux", "Microsoft", "Samsung"]);
+  const [programming, setProgramming] = useState(["Python", "C#"])
+
+  const deviceList = devices.map((device, index) => {
+    return <li style={{ margin: "30px" }} key={index}>{device}</li>;
+  });
+
+  const programmingList = programming.map((item, index)=>{
+    return <li key={index} style={{margin: "40px"}}>{item}</li>
+  })
+
+  function addDeviceName() {
+    setDevices([...devices, input.formOne]); // Add the input to the devices array
+    setInput(input = ""); // Clear the input field
+  }
+
+  function addLanguage() {
+    setProgramming([...programming, input.formTwo]); // Add the input to the programming array
+    setInput(input = ""); // Clear the input field
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        {/* <Button/>
-        <Label/> */}
-        <Form/>
-      </header>
-    </div>
+    <>
+      {deviceList}
+
+      <div>
+        <input type="text" value={input.formOne} onChange={(e) => setInput({...input, formOne: e.target.value})} />
+        <button onClick={addDeviceName}>Add Device</button>
+      </div>
+
+      {programmingList}
+
+      <div>
+        <input type="text" value={input.formTwo} onChange={(e) => setInput({...input, formTwo: e.target.value})} style={{marginBottom: "40px"}}/>
+        <button onClick={addLanguage}>Add Language</button>
+      </div>
+    </>
   );
-}
-
-export default App;
-
-
-function Button(){
-  const [click, setClick] = useState(0)
-
-  function handleClick(){
-    setClick(click + 1);
-  }
-
-  return (
-    <>
-      <button onClick={handleClick} style={{margin: "20px"}}>You Clicked the button {click}</button>
-    </>
-  )
-}
-
-
-function Label(){
-  const [value, setValue] = useState("");
-
-  function input(event){
-    setValue(event.target.value);
-  }
-
-  return(
-    <>
-      <input type="text" onChange={input} value={value} style={{margin: "20px"}}/>
-      <p>{value}</p>
-    </>
-  )
-}
-
-function Form(){
-  // Use State Variables
-  const [form, setForm] = useState({name: "", email: "", age: 0, isStudent: false, info: ""})
-  // the forms
-  return(
-    <form onSubmit={(e)=>{
-      e.preventDefault();
-      console.log(form)
-    }}>
-      {/* Name Form */}
-      <label>Name:</label>
-      <input type="text" value={form.name} onChange={(e)=>{setForm({...form, name: e.target.value})}}/>
-      <hr />
-      {/* Finished */}
-
-      {/* Email Form */}
-      <label>Email:</label>
-      <input type="email" value={form.email} onChange={(e)=>{setForm({...form, email: e.target.value})}}/>
-      <hr />
-      {/* Finished */}
-
-      {/* Age Form */}
-      <label>Age:</label>
-      <input type='number' value={form.age} onChange={(e)=>{setForm({...form, age: e.target.value})}}/>
-      <hr />
-      {/* Finished */}
-
-      {/* Checkbox Form */}
-      <label>Are You student</label>
-      <input type="checkbox" checked={form.isStudent} onChange={(e)=>{setForm({...form, isStudent: e.target.checked})}}/>
-      <hr />
-      {/* finished */}
-
-      {/* TextArea Form */}
-      <textarea name="" id="" value={form.info} onChange={(e)=>{setForm({...form, info: e.target.value})}}></textarea>
-      <hr />
-      {/* finished */}
-
-      <button type='submit'>submit</button>
-    </form>
-  )
-
 }
